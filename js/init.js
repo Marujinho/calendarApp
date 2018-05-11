6b00de08e89ce520e2e97728dcaebd1a6a5188f5
Marujinho <aka.contato@gmail.com>
2018-05-11 16:36:59 -0300

    arrumando os bugzin de instancias

--- js/init.js
+++ js/init.js
@@ -1,18 +1,18 @@
-$(function() {
-	var $title 	  = $('.brand-logo');
-	var $calendar = $('#content').fullCalendar({
-		header: false,
-		eventLimit: true, // allow "more" link when too many events
-		height: 'parent'
-    });
+// $(function() {
+// 	var $title 	  = $('.brand-logo');
+// 	var $calendar = $('#content').fullCalendar({
+// 		header: false,
+// 		eventLimit: true, // allow "more" link when too many events
+// 		height: 'parent'
+//     });
 	
-	$('.dropdown-trigger').dropdown();
+// 	$('.dropdown-trigger').dropdown();
 	
-	$title.text($calendar.fullCalendar('getDate').format(($calendar.fullCalendar('getDate').format('YYYY') == new Date().getFullYear()) ? 'MMMM' : 'MMM YYYY'));
+// 	$title.text($calendar.fullCalendar('getDate').format(($calendar.fullCalendar('getDate').format('YYYY') == new Date().getFullYear()) ? 'MMMM' : 'MMM YYYY'));
 	
-	$('nav ul li a.navigation').click(function() {
-		$calendar.fullCalendar($(this).attr('href').substring(1));
-		$title.text($calendar.fullCalendar('getDate').format(($calendar.fullCalendar('getDate').format('YYYY') == new Date().getFullYear()) ? 'MMMM' : 'MMM YYYY'));
-	});
-});
+// 	$('nav ul li a.navigation').click(function() {
+// 		$calendar.fullCalendar($(this).attr('href').substring(1));
+// 		$title.text($calendar.fullCalendar('getDate').format(($calendar.fullCalendar('getDate').format('YYYY') == new Date().getFullYear()) ? 'MMMM' : 'MMM YYYY'));
+// 	});
+// });
 
