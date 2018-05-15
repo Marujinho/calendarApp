189c986025dea1a80ec8060879b6fcfde90ff146
Marujinho <aka.contato@gmail.com>
2018-05-15 15:08:58 -0300

    service worker cache

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,43 @@ var cacheName = 'calendarAppV1';
 var filesToCache = [
 	// HTML
 	'./',
+	'js/angular.js',
+	'lib/angular/angular-ui-router.js',
+	'lib/angular/ngMask.js',
+	'config/routeRun.js',
+	'config/routeConfig.js',
+	//'jquery.min.js',
+	'js/fastclick.js',
+	'lib/materialize/css/materialize.min.css',
+	'css/easy_calendar.css',
+	'lib/fullcalendar/css/fullcalendar.min.css',
+	'lib/select2/css/select2.min.css',
+	'css/select2-materialize.css',
+	'lib/materialize/js/materialize.min.js',
+	'lib/select2/js/select2.min.js',
+	'lib/moment/js/moment.min.js',
+	'lib/fullcalendar/js/fullcalendar.min.js',
+	'lib/fullcalendar/js/pt-br.js',
+	//CONTROLLERS
+	'controllers/agendaCtrl.js',
+	'controllers/insertAppointmentCtrl.js',
+	'controllers/insertProfilesCtrl.js',
+	'controllers/insertProjectsCtrl.js',
+	'controllers/insertUsersCtrl.js',
+	'controllers/insertCustomersCtrl.js',
+	'controllers/closingDateCtrl.js',
+	'controllers/listCustomersCtrl.js',
+	'controllers/listAppointmentCtrl.js',
+	'controllers/listProfilesCtrl.js',
+	'controllers/listProjectsCtrl.js',
+	'controllers/listUsersCtrl.js',
+	'controllers/reportsCtrl.js',
+	'controllers/loginCtrl.js',
+	'factorys/controleDeLogin.js',
 	'index.html',
 	'./css/style.css',
-	'app.js'
+	'app.js',
+	'sss.js'
 ];
 
 self.addEventListener('install', function(e) {
@@ -18,14 +52,9 @@ self.addEventListener('install', function(e) {
 });
 
 
-
-
-
 self.addEventListener('activate',  event => {
 	event.waitUntil(self.clients.claim());
 });
-
-
 
 
 self.addEventListener('fetch', event => {
