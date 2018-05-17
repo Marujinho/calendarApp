9aae1111b29685cea09e6c20b41dcdd3cbbb6a66
Marujinho <aka.contato@gmail.com>
2018-05-17 11:36:22 -0300

    login

--- controllers/loginCtrl.js
+++ controllers/loginCtrl.js
@@ -1,7 +1,7 @@
 angularApp.controller('loginCtrl', function($scope, $rootScope, $state) {
     $rootScope.local = "";
     $rootScope.titulo = "LOGIN";
-    $scope.login = {};
+    // $scope.login = {};
     $rootScope.login = {};
 
 
