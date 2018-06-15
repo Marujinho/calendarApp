08c1f115f174bda20e231cd2d292a88c46cead84
Marujinho <aka.contato@gmail.com>
2018-06-15 11:06:17 -0300

    logout button

--- config/routeRun.js
+++ config/routeRun.js
@@ -1,6 +1,6 @@
 angularApp.run(function($rootScope, usersAPIService, $location) {
 	$rootScope.global = {
-		link: "https://deveasyprojectapi.iv2.com.br/EasyCalendar-1.0.0",
-		token: "dev3-ks4d-as42-83hk"
+		link: "https://demoeasyprojectapi.iv2.com.br/EasyCalendar-1.0.0",
+		token: "demo-54sa-da4w-ad42"
 	};
 });
