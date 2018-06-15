d6795dfa4364297a811dc20f83505fe916d3c424
Marujinho <aka.contato@gmail.com>
2018-06-15 16:18:25 -0300

    fazendo login ler parametros da url

--- config/routeRun.js
+++ config/routeRun.js
@@ -1,6 +1,6 @@
 angularApp.run(function($rootScope, usersAPIService, $location) {
 	$rootScope.global = {
-		link: "https://demoeasyprojectapi.iv2.com.br/EasyCalendar-1.0.0",
+		link: "https://deveasyprojectapi.iv2.com.br/EasyCalendar-1.0.0",
 		token: "demo-54sa-da4w-ad42"
 	};
 });
