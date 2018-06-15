a2baa5d184400bd43ed53d0ea7f2b8fa4d7c0de7
Marujinho <aka.contato@gmail.com>
2018-06-15 15:16:07 -0300

    logout button

--- controllers/loginCtrl.js
+++ controllers/loginCtrl.js
@@ -5,7 +5,7 @@ angularApp.controller('loginCtrl', function($scope, $rootScope, $state, $statePa
     $scope.param = $stateParams;
     alert($scope.param.id);
 
-    alert()
+    alert('oi2');
 
       var obj = $scope.param.id.replace('Y2lqZXZqZWRvYnJh', '');
       var obj2 = obj.replace('cGVyYXdhdGFua2Vsb21wb2twcm9wZXJ0aWl2Mg','');
