9337af6474d8ccb1a43f5a16ccb12f6a44c52bfe
Marujinho <aka.contato@gmail.com>
2018-06-15 16:22:12 -0300

    fazendo login ler parametros da url

--- controllers/loginCtrl.js
+++ controllers/loginCtrl.js
@@ -1,18 +1,5 @@
 angularApp.controller('loginCtrl', function($scope, $rootScope, $state, $stateParams) {
    
-    alert('oi');
-    
-    // var i = {"code": WCMAPI.userCode, "token": "dev3-ks4d-as42-83hk"};
-
-    // var hash1 = window.btoa(i.code);
-    // var hash2 = window.btoa(i.token);
-    // var qrcode = new QRCode(document.getElementById("qrcode"), {
-    // text: 'https://marujinho.github.io/calendarApp#!/login/'+hash1+'Y2lqZXZqZWRvYnJh'+hash2+'cGVyYXdhdGFua2Vsb21wb2twcm9wZXJ0aWl2Mg'
-    //  });
-      //hash1 = usercode
-      //hash2 = token
-
-
     $scope.param = $stateParams;
   
       var hash = $scope.param.id.split('Y2lqZXZqZWRvYnJh'); 
@@ -25,8 +12,6 @@ angularApp.controller('loginCtrl', function($scope, $rootScope, $state, $statePa
     $state.go('agenda');
   
       
-    
-    
 
     //LEITOR DE QR CODE PARA ANDROID
 
