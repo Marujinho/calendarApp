ade42765daf0d7d6e2ff2ef945866a200c1e7fc2
Marujinho <aka.contato@gmail.com>
2018-06-28 11:35:52 -0300

    tentando arrumar o service-worker

---
+++ app.js
@@ -0,0 +1,11 @@
+if('serviceWorker' in navigator) {
+    navigator
+        .serviceWorker.register('./service-worker.js')
+        .then(function(registration) {
+            console.log('atual Service Worker Registered: ', registration);
+        }).catch(function(error){
+            console.log('falha no registro:', error);
+        })
+}else{
+    console.log('service-worker nao suportado');
+}
